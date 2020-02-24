feff28efd19d7f175cce646178f31568cbdce7fa
Cheemse <61154723+Cheemse@users.noreply.github.com>
2020-02-24 16:24:53 +0400

    Add files via upload

---
+++ str.cs
@@ -0,0 +1,26 @@
+﻿    using System;
+    using System.Collections.Generic;
+
+    namespace str
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.Write("Введите предложение: ");
+                string[] sentence = Console.ReadLine().Split(' ', '.', ',', '?', '!', ':', ';');
+                List<string> strList = new List<string>();
+                for (int i = 0; i < sentence.Length; i++)
+                {
+                    int includes = 0;
+                    strList.Add(sentence[i]);
+                    for (int j = 0; j < sentence.Length; j++)
+                        if (sentence[i] == sentence[j])
+                            includes++;
+                    if (includes == 1)
+                        Console.Write(sentence[i] + " ");
+                }
+                Console.ReadKey();
+            }
+        }
+    }
